3e2577d19f11d121189d75ebb4cf879012e57f2a
Dayeeet <david@cappell.net>
2024-03-31 23:40:01 +0200

    refactor: init gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+rootProject.name = "ServerRegistrationPlugin"
+
+include("registration-shared", "registration-velocity", "registration-bungeecord")
+
