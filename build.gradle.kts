383338bf3cd91fbaaa85e196faf7e0c86d52a1ad
David <david@cappell.net>
2024-10-13 15:26:20 +0200

    feat: dev workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,10 +36,10 @@ subprojects {
 
 
     kotlin {
-        jvmToolchain(22)
+        jvmToolchain(21)
         compilerOptions {
             apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_22)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
         }
     }
 
