8bafbe7c6a8e96544a1083109f127150e89466ef
Philipp <p.eistrach@gmail.com>
2025-01-08 19:45:03 +0100

    fix: version name

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,13 @@ plugins {
     alias(libs.plugins.shadow)
 }
 
+val baseVersion = "0.0.1"
+val commitHash = System.getenv("COMMIT_HASH")
+val snapshotversion = "${baseVersion}-dev.$commitHash"
+
 allprojects {
     group = "app.simplecloud.plugin"
-    version = "0.0.1"
+    version = if (commitHash != null) snapshotversion else baseVersion
 
     repositories {
         mavenCentral()
