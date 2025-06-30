5378e785b106bebc8ee2590432b6d0e1e54fffbe
fllipeis <p.eistrach@gmail.com>
2025-06-30 17:49:34 +0200

    feat: 1.21.7

--- registration-bungeecord/build.gradle.kts
+++ registration-bungeecord/build.gradle.kts
@@ -30,6 +30,7 @@ modrinth {
         "1.21.4",
         "1.21.5",
         "1.21.6",
+        "1.21.7",
     )
     loaders.add("bungeecord")
     changelog.set("https://docs.simplecloud.app/changelog")
--- registration-velocity/build.gradle.kts
+++ registration-velocity/build.gradle.kts
@@ -56,6 +56,7 @@ modrinth {
         "1.21.4",
         "1.21.5",
         "1.21.6",
+        "1.21.7",
     )
     loaders.add("velocity")
     changelog.set("https://docs.simplecloud.app/changelog")
