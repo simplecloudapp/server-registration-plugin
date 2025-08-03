ff90312c9922e713a573a8f5a9ff8665309c8a07
sammy <53941165+coinsapi@users.noreply.github.com>
2025-08-03 11:21:11 +0200

    Update velocity version

--- registration-velocity/build.gradle.kts
+++ registration-velocity/build.gradle.kts
@@ -46,7 +46,11 @@ modrinth {
         "1.21.1",
         "1.21.2",
         "1.21.3",
-        "1.21.4"
+        "1.21.4",
+        "1.21.5",
+        "1.21.6",
+        "1.21.7",
+        "1.21.8"
     )
     loaders.add("velocity")
     changelog.set("https://docs.simplecloud.app/changelog")
