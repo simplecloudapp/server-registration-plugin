40bb4035a57cfe51e2b9e7621c8cd9363c14c1f7
Fynn Bauer <80108547+Kaseax@users.noreply.github.com>
2025-10-04 21:48:19 +0200

    feat: 1.21.9 support

--- registration-velocity/build.gradle.kts
+++ registration-velocity/build.gradle.kts
@@ -58,6 +58,7 @@ modrinth {
         "1.21.6",
         "1.21.7",
         "1.21.8",
+        "1.21.9",
     )
     loaders.add("velocity")
     changelog.set("https://docs.simplecloud.app/changelog")
