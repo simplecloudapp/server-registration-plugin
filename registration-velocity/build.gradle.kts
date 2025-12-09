4c264a87a4ad7afdb2cef09d22bddb0cd0751a80
CIMON <spacechunksstudio@gmail.com>
2025-12-09 17:58:55 +0000

    chore: add Minecraft version(s) 1.21.11

--- registration-velocity/build.gradle.kts
+++ registration-velocity/build.gradle.kts
@@ -43,6 +43,7 @@ modrinth {
     uploadFile.set(tasks.shadowJar)
     gameVersions.addAll(
         
+        
         "1.20",
         "1.20.1",
         "1.20.2",
@@ -61,6 +62,8 @@ modrinth {
         "1.21.8",
         "1.21.9",
         "1.21.10",
+        "1.21.11",
+
 
     )
     loaders.add("velocity")
